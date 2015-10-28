2e80eeca269b7aa9eec8921f1e5ea139437f0674
Martin Sojka <sojka.martin@gmail.com>
2015-10-28 15:20:12 +0100

    Finish 2nd part.

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,5 +1,33 @@
 window.Total = {}
 
-Total.view = function (ctrl) {
-  // View code goes here
+/* Model-level logic */
+Total.pricePerCount = 10
+Total.calcPrice = function(discount, count) {
+  var total = count * Total.pricePerCount
+  return roundCents(total - total * discount)
+}
+Total.calcDiscount = function(discount, count) {
+  var total = count * Total.pricePerCount
+  return roundCents(total * discount)
+}
+
+/* View */
+Total.view = function (ctrl, attrs) {
+  return m('p.total', [
+    m('span', 'Total: '),
+    discountView(ctrl, attrs),
+    m('strong', "$" + Total.calcPrice(attrs.discount, attrs.count))
+  ])
+}
+
+/* Helpers */
+function roundCents(num) {
+  return Math.round(num*100/100)
+}
+
+function discountView(ctrl, attrs) {
+  if(attrs.discount > 0) {
+    var discountedAmount = Total.calcDiscount(attrs.discount, attrs.count)
+    return m('span', "(Coupon discount: -$" + discountedAmount + ")")
+  }
 }
--- src/components/Volunteers.js
+++ src/components/Volunteers.js
@@ -2,9 +2,44 @@ window.Volunteers = {}
 
 Volunteers.controller = function (attrs) {
   var ctrl = this
-  // TODO: Controller code goes here
+  
+  ctrl.add = function() {
+    attrs.volunteers.push(Entry.volunteerVM())
+  }
+  ctrl.remove = function(idx) {
+    attrs.volunteers.splice(idx, 1)
+  }
 }
 
 Volunteers.view = function (ctrl, attrs) {
-  // TODO: View code goes here
+  return m('.volunteers', [
+    attrs.volunteers.map(function(volunteer, idx) {
+      return m('fieldset', [
+        m('legend', "Volunteer #" + (idx+1)),
+        m('label', "Name"),
+        m('input[type=text]', {
+          value: volunteer.name,
+          onchange: function(e) {
+            volunteer.name = e.currentTarget.value
+          }
+        }),
+        m('br'),
+        m('label', "Email"),
+        m('input[type=text]', {
+          value: volunteer.email,
+          onchange: function(e) {
+            volunteer.email = e.currentTarget.value
+          }
+        }),
+        removeAnchor(ctrl, attrs, idx)
+      ])
+    }),
+    m('button', {onclick: ctrl.add}, 'Add another volunteer')
+  ])
+}
+
+function removeAnchor(ctrl, attrs, idx) {
+  if(attrs.volunteers.length >= 2) {
+    return m('button', {onclick: ctrl.remove.papp(idx)}, 'remove')
+  }
 }
